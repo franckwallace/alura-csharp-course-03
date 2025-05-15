b4823d69023b54565f444e0727c8223b6d9d09f2
Franck Wallace <franckwallace.fw@outlook.com>
2025-05-14 23:15:27 -0300

    testes com exceção

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -11,26 +11,50 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            // nova forma de se construir um objeto, obedecendo o estabelecido no Construtor-01 na classe ContaCorrente
-            // ContaCorrente conta = new ContaCorrente(-263, 263105);
+
+            //// nova forma de se construir um objeto, obedecendo o estabelecido no Construtor-01 na classe ContaCorrente
+            //// ContaCorrente conta = new ContaCorrente(-263, 263105);
 
 
-            // Console.WriteLine(conta.Agencia);
-            // Console.WriteLine(conta.Numero);
-
-            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
-
-            ContaCorrente contaDoBruno = new ContaCorrente(263, 263105);
-
-            Console.WriteLine(ContaCorrente.TaxaOperacao);
+            //// Console.WriteLine(conta.Agencia);
+            //// Console.WriteLine(conta.Numero);
 
             //Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
 
-            //ContaCorrente contaDaGabriela = new ContaCorrente(263, 263020);
+            //ContaCorrente contaDoBruno = new ContaCorrente(263, 263105);
 
-            //Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+            //Console.WriteLine(ContaCorrente.TaxaOperacao);
+
+            ////Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            ////ContaCorrente contaDaGabriela = new ContaCorrente(263, 263020);
+
+            ////Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            Metodo();
 
             Console.ReadLine();
         }
+
+        // Teste com a cadeia em chamars;
+        //Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            //TestaDivisao(0);
+            TestaDivisao(2);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado );
+        }
+
+        private static int Dividir (int numero, int divisor)
+        {
+            return numero / divisor;
+        }
+
     }
 }
